dbdce4084518cbbfd95fcabe0235343f302a592e
shalu-mishra <mishravimalmishra587@gmail.com>
2026-05-19 18:22:13 +0530

    Fix CORS issue

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://appointment-management-system-ekbu.onrender.com",
+  credentials: true
+}));
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/authRoutes'));
