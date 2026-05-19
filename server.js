8f80d3b4c236e777887494c868f9d6059c8b873c
shalu-mishra <mishravimalmishra587@gmail.com>
2026-05-19 18:26:40 +0530

    Enable CORS for frontend

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const app = express();
 
 app.use(cors({
   origin: "https://appointment-management-system-ekbu.onrender.com",
+  methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
 app.use(express.json());
